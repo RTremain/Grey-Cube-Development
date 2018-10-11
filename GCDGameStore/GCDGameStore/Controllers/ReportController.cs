6ee5b231fe243f487d1b403696140ddf84a8b4e9
Mitchell Duggan <Mduggan0602@conestogac.on.ca>
2018-10-11 11:04:58 -0400

    Annotated models, Started report controller, controller header documentation added

---
+++ GCDGameStore/GCDGameStore/Controllers/ReportController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GCDGameStore.Models;
+
+namespace GCDGameStore.Controllers
+{
+    public class ReportController : Controller
+    {
+        private readonly GcdGameStoreContext _context;
+
+        public ReportController(GcdGameStoreContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employee.ToListAsync());
+        }
+    }
+}
--- GCDGameStore/GCDGameStore/Models/Game.cs
+++ GCDGameStore/GCDGameStore/Models/Game.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GCDGameStore.Models
 {
     public class Game
     {
         public int GameID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
         public DateTime ReleaseDate { get; set; }
     }
 }
