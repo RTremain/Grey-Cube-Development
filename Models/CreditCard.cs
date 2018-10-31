3efed0798bdad02d7a6eea704850b0f38601f359
maoqiu1105 <wangqiao921105@gmail.com>
2018-10-31 13:22:01 -0400

    edit models for validation

---
+++ Models/CreditCard.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCDGameStore.Models
+{
+    public class CreditCard
+    {
+        public int CreditCardId { get; set; }
+
+        [Required]
+        [CreditCard]
+        public string CcNum { get; set; }
+
+        [Range(1, 12)]
+        public int ExpMonth { get; set; }
+
+        // TODO: PROPER VALIDATION
+        [Range(2018, 2028)]
+        public int ExpYear { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Province { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [ForeignKey("MemberId")]
+        public int MemberId { get; set; }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GCDGameStore.Models
+{
+    public partial class Event
+    {
+        public int EventId { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime EventDate { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
+        public string Description { get; set; }
+    }
+}
