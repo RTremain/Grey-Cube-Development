8a30d663aec061ab8523288a89454454af9b6aae
Mitchell Duggan <Mduggan0602@conestogac.on.ca>
2018-10-31 13:51:47 -0400

    Changes to models

--- GCDGameStore/GCDGameStore/Models/Friend.cs
+++ GCDGameStore/GCDGameStore/Models/Friend.cs
@@ -7,5 +7,15 @@ using System.Threading.Tasks;
 
 namespace GCDGameStore.Models
 {
-    
+    public class Friend
+    {
+        public int FriendId { get; set; }
+
+        public int MemberId { get; set; }
+        public Member Member { get; set; }
+
+        public int FriendMemberId { get; set; }
+        public Member FriendMember { get; set; }
+    }
+
 }
